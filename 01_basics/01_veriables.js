1790ff90c21e0d16c01133762f70c0e70fc0e58f
GirishV7 <163229593+GirishV7@users.noreply.github.com>
2026-03-27 17:16:34 +0000

    datatypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,12 @@
+const accid=123455
+let email="abc@gmail.com";
+var password="123456";
+city ="Delhi";
+// let area;
+//accid=2;
+email="xyz@gmail.com"
+password="87876786";
+city="Mumbai";
+
+console.log(accid);
+console.table({email,password,city})
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+const accid=123455
+let email="abc@gmail.com";
+var password="123456";
+city ="Delhi";
+
+console.log(typeof(city));
+console.log(typeof(accid));

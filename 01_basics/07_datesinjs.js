be6a0a34cf3401a3c633057595ffd98cd4e45b51
GirishV7 <163229593+GirishV7@users.noreply.github.com>
2026-04-06 07:39:05 +0000

    dates

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,16 @@
+const mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.toString());
+
+// console.log(mydate.toDateString());
+
+// console.log(mydate.toLocaleDateString());
+
+// console.log(mydate.toJSON());
+
+// console.log(mydate.toLocaleString());
+
+// console.log(mydate.toLocaleTimeString());
+
+let create= new Date(2003,3,25);
+console.log(create.toDateString());

955a2b3e137953f41456cd4cc384f99a276a0a49
GirishV7 <163229593+GirishV7@users.noreply.github.com>
2026-04-06 07:15:44 +0000

    num and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,31 @@
+const num=400;
+// console.log(num);
+
+const val= new Number(100);
+// console.log(val);
+
+// console.log(val.toString());
+
+// console.log(val.toFixed(2));
+const numeric=11123.897454432451;
+// console.log(numeric.toPrecision(3));
+
+const hunders= 100000000;
+// console.log(hunders.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++Maths++++++++++++++++++++++++++++++++//
+
+// console.log(Math);
+
+// console.log(Math.round(3.5));
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min)+1)+min);
